c768ac3f81d2c2cea26410af8b6cac95093abce9
Dustin Hoeppner <dhoepp@me.com>
2025-08-01 11:45:24 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li>
+          <li className="tracking-[-.01em]">
             Hello Blake
           </li>
         </ol>
